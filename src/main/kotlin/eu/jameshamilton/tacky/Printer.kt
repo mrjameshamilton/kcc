aac690ca85a5bf2875a426e875361369e34f474e
james <mrjameshamilton@gmail.com>
2024-09-02 20:17:15 +0200

    Print double tacky constants

--- src/main/kotlin/eu/jameshamilton/tacky/Printer.kt
+++ src/main/kotlin/eu/jameshamilton/tacky/Printer.kt
@@ -209,6 +209,7 @@ private fun printTacky(value: Value, printType: Boolean = true) {
             is Long -> print("long ${value.value}")
             is UInt -> print("unsigned int ${value.value}")
             is ULong -> print("unsigned long ${value.value}")
+            is Double -> print("double ${value.value}")
         }
 
         is Var -> if (printType) {
